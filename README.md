c0b840b45c9559036e823086dcca5bf3cd5a6ac6
Ag <aravindmallapureddy@juspay.in>
2021-09-07 02:02:03 +0530

    Added Auth API's to the tutorial examples.

--- README.md
+++ README.md
@@ -185,8 +185,23 @@ getWith opts "https://httpbin.org/basic-auth/user/pass"
 
 ### Auth headers using Bearer Token.
 ```haskell
-let opts = defaults & auth ?~ oauth2Bearer "1234abcd"
-getWith opts "https://httpbin.org/basic-auth/user/pass"
+  -- generated this bearer with curl /v1/token at: https://instantwebtools.net/secured-fake-rest-api
+let opts = defaults & auth ?~ oauth2Bearer "eyJraWQiOiJQZjNoZlFEYVdYM0dMekJ2ZHk3MkhzVExRWUhKbk45dldFQm1LLUItUFJvIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULnJjbHppTGR3QUtRdUQ1X3R2Smk1ZnpKQ3BTLXV2WWFiQVE4d0pUb01YRnMub2FyY2p1czlucDJVdHE0MUY0eDYiLCJpc3MiOiJodHRwczovL2Rldi00NTc5MzEub2t0YS5jb20vb2F1dGgyL2F1c2hkNGM5NVF0RkhzZld0NHg2IiwiYXVkIjoiYXBpIiwiaWF0IjoxNjMwOTU3NzAwLCJleHAiOjE2MzA5NjEzMDAsImNpZCI6IjBvYWhkaGprdXRhR2NJSzJNNHg2IiwidWlkIjoiMDB1aGVuaDFwVkRNZzJ1ZXg0eDYiLCJzY3AiOlsib2ZmbGluZV9hY2Nlc3MiXSwic3ViIjoiYXBpLXVzZXI0QGl3dC5uZXQifQ.EXwgGLVIpHvDObOyP5Xud_ayZW5Y3UuA7evDGVGog8EEFgr0nYw51WQLsrwbWjA9SYb-94822yn9gaJt74hVksAL9XKpfzTS1wIMgIKZjQCTh3XOX5167H5BHkHpkaATNTd4G4wjpWfTjClEKwpmGEMEeIAKQ-vTZgGCU5H5ABLNNpcrwDBLvWddLNSny1zABXdXDx1N4OYj8vglv7T27LljUjPwAZNy-fFF3s24fzf0EyPK7A9WngyYS1o_Ry_1PjaFIeIG0G3LjTtgoX8Kcan_CZrIpV6K8m619Ll-Jzon03aDJj8LNm8npcRhgemNDUUAHavUdb1pAvqquaIzhQ"
+resp <- getWith opts "https://api.instantwebtools.net/v2/airlines"
+return $ resp ^. responseBody
+
+RespBody:
+{
+  "id": 10070,
+  "name": "Srii Lankan Airways",
+  "country": "Sri Lanka",
+  "logo": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/sri_lanka.png",
+  "slogan": "From Sri Lanka",
+  "head_quaters": "Katunayake, Sri Lanka",
+  "website": "www.srilankaairways.com",
+  "established": "1990"
+}
+
 ```
 You can try other ways of Auth using the primitivies as Above
 
--- app/Main.hs
+++ app/Main.hs
@@ -16,6 +16,7 @@ main = do
   print =<< simpleGetWith
   print =<< simplePost
   print =<< simplePostWith
+  print =<< authGet
   putStrLn "Tutorial ended..."
 
 simpleGet :: IO ByteString
@@ -29,7 +30,6 @@ simpleGetWith = do
   let opts = defaults & header "Accept" .~ ["*/*"]
   resp <- getWith opts "http://httpbin.org/get"
   return $ resp ^. responseBody
-
 data ExRequest = ExRequest
   { num :: Int
   , str :: String
@@ -47,4 +47,11 @@ simplePostWith = do
   let opts = defaults & param "foo" .~ ["bar", "quux"]
   let req = ExRequest 1 "one"
   resp <- postWith opts "http://httpbin.org/post" (toJSON req)
+  return $ resp ^. responseBody
+
+authGet :: IO ByteString
+authGet = do
+  -- generated this bearer with curl /v1/token at: https://instantwebtools.net/secured-fake-rest-api
+  let opts = defaults & auth ?~ oauth2Bearer "eyJraWQiOiJQZjNoZlFEYVdYM0dMekJ2ZHk3MkhzVExRWUhKbk45dldFQm1LLUItUFJvIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULnJjbHppTGR3QUtRdUQ1X3R2Smk1ZnpKQ3BTLXV2WWFiQVE4d0pUb01YRnMub2FyY2p1czlucDJVdHE0MUY0eDYiLCJpc3MiOiJodHRwczovL2Rldi00NTc5MzEub2t0YS5jb20vb2F1dGgyL2F1c2hkNGM5NVF0RkhzZld0NHg2IiwiYXVkIjoiYXBpIiwiaWF0IjoxNjMwOTU3NzAwLCJleHAiOjE2MzA5NjEzMDAsImNpZCI6IjBvYWhkaGprdXRhR2NJSzJNNHg2IiwidWlkIjoiMDB1aGVuaDFwVkRNZzJ1ZXg0eDYiLCJzY3AiOlsib2ZmbGluZV9hY2Nlc3MiXSwic3ViIjoiYXBpLXVzZXI0QGl3dC5uZXQifQ.EXwgGLVIpHvDObOyP5Xud_ayZW5Y3UuA7evDGVGog8EEFgr0nYw51WQLsrwbWjA9SYb-94822yn9gaJt74hVksAL9XKpfzTS1wIMgIKZjQCTh3XOX5167H5BHkHpkaATNTd4G4wjpWfTjClEKwpmGEMEeIAKQ-vTZgGCU5H5ABLNNpcrwDBLvWddLNSny1zABXdXDx1N4OYj8vglv7T27LljUjPwAZNy-fFF3s24fzf0EyPK7A9WngyYS1o_Ry_1PjaFIeIG0G3LjTtgoX8Kcan_CZrIpV6K8m619Ll-Jzon03aDJj8LNm8npcRhgemNDUUAHavUdb1pAvqquaIzhQ"
+  resp <- getWith opts "https://api.instantwebtools.net/v2/airlines"
   return $ resp ^. responseBody
